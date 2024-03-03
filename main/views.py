ba3874968e315111cce431d9292529a8b14afeae
Arsen(Kusik) <arsenahaev052@gmail.com>
2024-03-03 18:40:04 +0200

    Second comit (just maketing from You tube and changing for myself)

--- main/views.py
+++ main/views.py
@@ -1,16 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import context
+
 
 # Create your views here.
 def index(request) -> HttpResponse:
      context={
     'title':'Home',
     'content':'Main Stranica-Home',
-    'dict':{'asd':1},
-    'Bool':True
+ 
     }
      return render(request,'main/index.html',context)
 
+
 def about(request) -> HttpResponse:
-    return HttpResponse('About Page')
+     context={
+    'title':'Home-О нас',
+    'content':'о нас',
+    'text_on_page':"lorem5"
+    }
+     return render(request,'main/about.html',context)
